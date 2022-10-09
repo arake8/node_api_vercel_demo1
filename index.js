3fca4f7e2cedf4140aaa4777e6bd193d5cd58002
arake8 <kitsana.wkk@gmail.com>
2022-10-09 17:03:32 +0700

    add sql function

--- index.js
+++ index.js
@@ -24,6 +24,104 @@ let dbCon = mysql.createConnection({
 });
 dbCon.connect();
 
+// retrieve all books
+app.get('/books', (req, res) => {
+    dbCon.query('SELECT * FROM books', (error, results, fields) => {
+        if(error) throw error;
+        
+        let message = "";
+        if(results === undefined || results.length == 0){
+            message = "Books table is empty";
+        }else{
+            message = "Successfully retrieved all books";
+        }
+        return res.send({error: false, data: results, message: message});
+    })
+});
+
+// add a new book
+app.post('/book', (req, res) => {
+    let name = req.body.name;
+    let author = req.body.author;
+
+    // validation
+    if(!name || !author){
+        return res.status(400).send({ error: true, message: "Please provide book name and author."})
+    }else{
+        dbCon.query('INSERT INTO books (name, author) VALUES(?, ?)', [name, author], (error, results, fields) =>{
+            if(error) throw error;
+            return res.send({ error: false, data: results, message: "Book successfully added"})
+        })
+    }
+});
+
+// retrieve books by id
+app.get('/book/:id', (req, res)=>{
+    let id = req.params.id;
+
+    if(!id){
+        return res.status(400).send({ error: true, message: "Please provide book id"});
+    } else {
+        dbCon.query("SELECT * FROM books WHERE id = ?", id, (error, results, fields) =>{
+            if (error) throw error;
+
+            let message = "";
+            if(results === undefined || results.length == 0){
+                message = "Books not found";
+            } else {
+                message = "Successfully retrieved book data";
+            }
+
+            return res.send({ error: false, data: results, message: message});
+        });
+    }
+});
+
+// update data book with id
+app.put('/book', (req, res) => {
+    let id = req.body.id;
+    let name = req.body.name;
+    let author = req.body.author;
+
+    // validation
+    if(!id || !name || !author){
+        return res.status(400).send({ error: true, message: 'Please provide book id, name and author'});
+    } else {
+        dbCon.query('UPDATE books SET name = ?, author = ? WHERE id = ?', [name, author, id], (error, results, fields) => {
+            if (error) throw error;
+
+            let message = "";
+            if(results.changeRows === 0){  // changeRows จำนวรการเปลี่ยนแปลงข้อมูล
+                message = "Books not found or data are same";
+            } else {
+                message = "Books successfully updated";
+            }
+            return res.send({ error: false, data: results, message: message });
+        });
+    }
+});
+
+// delete book by id
+app.delete('/book', (req, res)=>{
+    let id = req.body.id;
+
+    if (!id){
+        return res.status(400).send({ error: true, message: "Please provide book id"});
+    } else {
+        dbCon.query('DELETE FROM books WHERE id = ?', [id], (error, results, fields) => {
+            if (error) throw error;
+
+            let message = "";
+            if(results.affectedRows === 0){ // affectedRows จำนวนการมีผลต่อข้อมูล
+                message = "Book not found";
+            } else {
+                message = "Book successfully deleted";
+            }
+            return res.send({ error: false, data: results, message:message });
+        });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Node App is running on port ${PORT}`);
 });
